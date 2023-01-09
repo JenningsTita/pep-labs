bab9e3995fc2787fffd567d8d594312117ca2243
JenningsTita <ganatita@gmail.com>
2023-01-09 00:36:17 +0000

    Saved progress on CombineStrings 2023-01-09 00:36:17 +0000

--- CombineStrings/src/main/java/CombineStrings.java
+++ CombineStrings/src/main/java/CombineStrings.java
@@ -7,7 +7,8 @@ public class CombineStrings {
      * @param b the second part of the string to be combined.
      * @return the combination of strings a and b.
      */
-    public String combine(String a, String b){
-        return "";
+    public void Stringscombine(String a, String b){
+        return ""; 
+         
     }
 }
