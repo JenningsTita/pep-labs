b3bb2b8cca23a11d1799a31ba46d622a29dd69e3
JenningsTita <ganatita@gmail.com>
2023-01-10 01:26:28 +0000

    Saved progress on IfStatement 2023-01-10 01:26:28 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -16,7 +16,7 @@ public class IfStatement {
      * @param y second number to be compared.
      * @return x if bool is true. if bool is false, return y.
      */
-    public int ifExample(boolean bool, int x, int y){
-        return 0;
+    public int ifExample(boolean bool,int x, int y){
+        return is;
     }
 }
