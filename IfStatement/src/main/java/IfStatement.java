b39d3a092c516e9e123b9295f3841695012943b4
JenningsTita <ganatita@gmail.com>
2023-01-10 03:30:34 +0000

    Saved progress on IfStatement 2023-01-10 03:30:34 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -14,9 +14,9 @@ public class IfStatement {
      * @param bool a true/false value that determines if x or y is to be returned.
      * @param x first number to be compared.
      * @param y second number to be compared.
-     * @return x if bool is true. if bool is false, return y.
+     * @param i TODO
      */
-    public int ifExample(boolean bool,int x, int y){
-        return is;
+    public  ifExample (boolean bool,int x, int y){
+        return ;
     }
 }
